81e5e0a04f368ba259bf54c8516c05284990fe35
alfiiasharipova <alfiiasharipova@gmail.com>
2020-10-27 23:36:32 +0300

    Project created

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,8 @@
+public class MoneyTransfer {
+    public static void main(String[] args){
+        int bankAccount = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = bankAccount + transfer;
+        System.out.println(total);
+    }
+}
